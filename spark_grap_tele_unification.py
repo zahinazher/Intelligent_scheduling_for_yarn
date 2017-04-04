1ac7e6211f9e7762eecbf0e19034fcfa47d4520c
zahinazher <zahinazher@gmail.com>
2017-04-05 01:39:51 +0200

    added some functions

--- spark_grap_tele_unification.py
+++ spark_grap_tele_unification.py
@@ -2,6 +2,7 @@ from pyspark import SparkConf, SparkContext
 from influxdb import InfluxDBClient
 from copy import deepcopy
 import tensorflow as tf
+import re
 
 timestamps = [
     1490706976000000000,
@@ -17,6 +18,7 @@ class InfluxTensorflow():
         self.username = username
         self.password = password
         self.db = db
+        self.use_spark = False
         
 
         #'/nodemanager.container.ContainerResource_container_.*.ContainerResource=container_.*.Context=container.ContainerPid=.*.Hostname=vagrant.PCpuUsagePercentMaxPercents/,'+\
@@ -35,18 +37,18 @@ class InfluxTensorflow():
         '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AllocatedContainers",'\
         '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AvailableVCores",'+\
         '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.ContainerLaunchDurationAvgTime" '
-        
-        self.query_t = 'select * from cpu,"mem" where time = 1490706992000000000 '
 
-    def query_batch(self, q, limit, offset, db, epoch='ms'):
+        self.query_t = 'select * from cpu,mem '# where time = 1490706992000000000'
+
+    def query_batch(self, query, db, epoch='ns'):
         cli = InfluxDBClient(self.hostname, self.port, self.username, self.password)
+
         while True:
-            qq = "{0} limit {1} offset {2}".format(q, limit, offset)
-            res = cli.query(qq, database=db, epoch=epoch)
+            res = cli.query(query, database=db, epoch=epoch)
             if not res:
                 break
-            yield res
-            #return res
+            #yield res
+            return res
 
             offset += limit
             break
@@ -55,29 +57,57 @@ class InfluxTensorflow():
         X = tf.placeholder(tf.float32, data)
         print X
 
+    def add_column_name_to_data(self, values, value):
+        return [val+[value] for val in values]
+
+    def extract_time_list(self, values):
+        return [val[0] for val in values]
+
     def main(self):
 
-        for res in self.query_batch(self.query_g, limit=50, offset=50, db="graphite"):
-            print res
-
-
-        #print ("Result: {0}".format(result_t))
-        #print ("Result: {0}".format(result_g.items))
-
-        #sc = SparkContext()
+        limit = 5
+        offset = 5
+        query = "{0} limit {1} offset {2}".format(self.query_g, limit, offset)
+        results_g = self.query_batch(query, db="graphite")
         count = 0
-        """for tab in result.raw['series']:
-            values = tab['values']
-            name = tab['name']
-            columns = tab['columns']
-            rdd = (sc.parallelize(values)) # .values and .keys
-            #print rdd.collect()
-            if count > 0:
-                res = res.join(rdd)
+
+        if self.use_spark:
+            sc = SparkContext()
+
+        for res_g in results_g.raw['series']:
+            values = res_g['values']
+            name = res_g['name']
+            #columns = a['columns']  # value and time
+            if re.search(r'yarn.Hostname=(.*?)\.',name,re.I|re.S):
+                hostname = re.search(r'yarn.Hostname=(.*?)\.',name,re.I|re.S).group(1)
+                print "hostname:",hostname
+                #values = self.add_column_name_to_data(values=values, value=hostname)
+                time_list = self.extract_time_list(values)
+                for tm in time_list[0:]:
+                    print tm
+                    query = "{0} where time = {1} limit {2} offset {3}".format(self.query_t, tm, 2, 2)
+                    results_t = self.query_batch(query, db="telegraf")
+                    for res_t in results_t.raw['series']:
+                        values_t = res_t['values']
+                        name_t = res_t['name']
+
+                        val_lim = [value[:1]+value[21:23] for value in values_t]
+                        print val_lim
+
+            if self.use_spark:
+                rdd = (sc.parallelize(values)) # .values and .keys  #.map(lambda x: 2 * x))
+                #print rdd.collect()
+
+            if self.use_spark:
+                if count > 0:
+                    res = res.join(rdd)
+                else:
+                    res = rdd
+                count += 1
             else:
-                res = rdd
-            count += 1
-        print (res.collect())"""
+                print values
+
+        #print ("Result: {0}".format(result_g.items))
 
         """
         count0 = 0
