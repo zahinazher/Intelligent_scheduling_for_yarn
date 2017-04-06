d6f9ab96c488f6b611e37a2d9e082606f7bda3d5
zahinazher <zahinazher@gmail.com>
2017-04-06 23:55:51 +0200

    data ready to feed tensorflow

--- spark_grap_tele_unification.py
+++ spark_grap_tele_unification.py
@@ -32,10 +32,31 @@ class InfluxTensorflow():
         #'"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemNonHeapUsedM",'+\
         #'"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AllocatedGB",'+\
 
+
+        self.query_gg = 'select * from '+\
+         '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AllocatedContainers",'+\
+        '/nodemanager.container.ContainerResource_container_.*.ContainerResource=container_.*.Context=container.ContainerPid=.*.Hostname=vagrant.PCpuUsagePercentMaxPercents/,'+\
+        '/nodemanager.container.ContainerResource_container_.*.ContainerResource=container_.*.Context=container.ContainerPid=.*.Hostname=vagrant.PMemUsageMBsMaxMBs/,'+\
+        '/application_.*.driver.BlockManager.memory.memUsed_MB/,/application_.*.driver.BlockManager.memory.remainingMem_MB/,'+\
+        '/application_.*.driver.BlockManager.disk.diskSpaceUsed_MB/,'+\
+        '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemHeapCommittedM",'+\
+        '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemHeapMaxM",'+\
+        '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemHeapUsedM",'+\
+        '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemNonHeapCommittedM",'+\
+        '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemNonHeapMaxM",'+\
+        '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemNonHeapUsedM",'+\
+        '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AllocatedGB",'+\
+         '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.ContainerLaunchDurationAvgTime" '
+
+
         self.query_g = 'select * from '+\
-        '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AllocatedContainers",'+\
-        '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AvailableVCores",'+\
-        '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.ContainerLaunchDurationAvgTime" '
+         '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AllocatedContainers",'+\
+         '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AvailableVCores",'+\
+         '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemNonHeapCommittedM",'+\
+         '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemNonHeapMaxM",'+\
+         '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemNonHeapUsedM",'+\
+         '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AllocatedGB",'+\
+         '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.ContainerLaunchDurationAvgTime" '
 
         self.query_t = 'select * from cpu,mem '
 
@@ -62,10 +83,12 @@ class InfluxTensorflow():
     def extract_time_list(self, values):
         return [val[0] for val in values]
 
-    def join_rdd(self, rdd1_, rdd2):
+    def join_rdd(self, rdd1_, rdd2_):
         rdd_ = rdd1_.join(rdd2_) #.collectAsMap() # .reduceByKey(lambda x,y : x+y)
-        #print 'rdd_',rdd_.collect()
-        return rdd_.map(lambda x : (x[0],sum(x[1],()))) # .coalesce().collect()
+        if rdd_:
+            return rdd_.map(lambda x : (x[0],sum(x[1],())))
+        else:
+            return rdd1_
 
     def main(self):
 
@@ -82,31 +105,29 @@ class InfluxTensorflow():
         sc = SparkContext()
 
         count = 0
-        for res_g in results_g.raw['series'][:3]:
+        for res_g in results_g.raw['series'][0:]:
             values_g = res_g['values']
             name_g = res_g['name']
-            #print 'values',values_g
 
             rdd1 = sc.parallelize(values_g)
             rdd1_ = rdd1.map(lambda x: (x[0], tuple(x[1:])))
+            print rdd1_.collect()
             if count == 0:
-                rdd1_old = rdd1_
-            elif count == 1:
-                #rdd1_old = self.join_rdd(rdd1_old, rdd1_)
-                rdd2 = sc.parallelize(values_g)
-                rdd2_ = rdd2.map(lambda x: (x[0], tuple(x[1:])))
-                print 'rdd2_',rdd2_.collect()
+                rdd_join = rdd1_
             else:
-                rdd_ = rdd1_.join(rdd2_) #.collectAsMap() # .reduceByKey(lambda x,y : x+y)
-                print 'rdd_',rdd_.collect()
-                rdd = rdd_.map(lambda x : (x[0],sum(x[1],())))
-                print 'rddjoin',rdd.collect()
-
+                rdd_join = self.join_rdd(rdd_join, rdd1_)
+                pass
             count += 1
-        #print rdd1_old.collect()
+        print rdd_join.collect()
+        #print rdd_join.coalesce(1).collect()   # .glom()
 
-        #rdd_map = self.join_rdd(rdd1, rdd2)
-        #print rdd_map.collect()
+        """for res_t in results_t.raw['series'][:2]:
+            values_t = res_t['values']
+            name_t = res_t['name']
+            rdd1 = sc.parallelize(values_t)
+            rdd1_ = rdd1.map(lambda x: (x[0], tuple(x[1:])))
+            rdd_join = self.join_rdd(rdd_join, rdd1_)
+        print rdd_join.collect()"""
 
         #columns = a['columns']  # value and time
         if re.search(r'yarn.Hostname=(.*?)\.',name_g,re.I|re.S):
@@ -115,41 +136,8 @@ class InfluxTensorflow():
             #values = self.add_column_name_to_data(values=values, value=hostname)
             time_list = self.extract_time_list(values_g)
 
-            """for tm in time_list[0:]:
-                print tm
-                query = "{0} where time = {1} limit {2} offset {3}".format(self.query_t, tm, 2, 2)
-                results_t = self.query_batch(query, db="telegraf")
-                for res_t in results_t.raw['series']:
-                    values_t = res_t['values']
-                    name_t = res_t['name']
+        #self.load_data_into_tensorflow(rdd_join.collect())
 
-                    val_lim = [value[:1]+value[21:23] for value in values_t]
-                    print 'tele',val_lim
-            """
-
-        """
-        count0 = 0
-        for tab in result_t.raw['series']:
-            values = tab['values']
-            name = tab['columns']
-            val_list = list()
-            for value in values:
-                val = value[:1]+value[21:23]
-                val_list.append(val)
-            val_list0 = deepcopy(val_list)
-            rdd = (sc.parallelize(val_list)) #.map(lambda x: 2 * x))
-            #print rdd.collect()
-            if count0 > 0:
-                res1 = res1.join(rdd)
-            else:
-                res1 = rdd
-            count0 += 1
-
-        print (res1.collect())
-        """
-        #self.load_data_into_tensorflow(res1.collect())
-
-        #print (sc.parallelize([0, 2, 3, 4, 6], 5).glom().collect())
 
 if __name__ == '__main__':
     indbtf = InfluxTensorflow('localhost', 8086, 'adminuser', 'adminpw', 'graphite')
