8ff47cee9845ed2628a2225cd7661a6233c9c37d
zahinazher <zahinazher@gmail.com>
2017-04-11 12:11:18 +0200

    RDD in list in a list format

--- spark_grap_tele_unification.py
+++ spark_grap_tele_unification.py
@@ -2,6 +2,7 @@ from pyspark import SparkConf, SparkContext
 from influxdb import InfluxDBClient
 from copy import deepcopy
 import tensorflow as tf
+import os
 import re
 from operator import add
 
@@ -52,6 +53,8 @@ class InfluxTensorflow():
         self.query_g = 'select * from '+\
          '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AllocatedContainers",'+\
          '"nodemanager.yarn.NodeManagerMetrics.Context=yarn.Hostname=vagrant.AvailableVCores",'+\
+         '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemHeapMaxM",'+\
+         '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemHeapUsedM",'+\
          '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemNonHeapCommittedM",'+\
          '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemNonHeapMaxM",'+\
          '"nodemanager.jvm.JvmMetrics.Context=jvm.ProcessName=NodeManager.Hostname=vagrant.MemNonHeapUsedM",'+\
@@ -73,12 +76,20 @@ class InfluxTensorflow():
             offset += limit
             break
 
-    def training_step(i, update_test_data, update_train_data):
+    def training_step(i, update_test_data, update_train_data, XX, Y_, rdd_join):
 
         print "\r", i,
         ####### actual learning
         # reading batches of 100 images with 100 labels
-        batch_X, batch_Y = mnist.train.next_batch(100)
+        #batch_X, batch_Y = mnist.train.next_batch(100)
+        batch_X = [[ 1,  0,  0,  1,  0,  1,  0,  0,  0,  0]
+ [ 0,  0,  0,  1,  0,  0,  0,  0,  0,  0]
+ [ 0,  1,  0,  0,  0,  0,  1,  0,  0,  0]]
+
+        batch_Y = [[ 1,  0,  0,  1,  0,  1,  0,  1,  0,  0]
+ [ 0,  0,  0,  1,  1,  0,  1,  0,  0,  0]
+ [ 0,  1,  0,  0,  1,  0,  1,  0,  0,  0]]
+
         # the backpropagation training step
         sess.run(train_step, feed_dict={XX: batch_X, Y_: batch_Y})
 
@@ -93,39 +104,40 @@ class InfluxTensorflow():
             train_a.append(a)
             train_c.append(c)
 
-        if update_test_data:
+        """if update_test_data:
             a, c = sess.run([accuracy, cross_entropy], feed_dict={XX: mnist.test.images, Y_: mnist.test.labels})
             test_a.append(a)
-            test_c.append(c)
-
+            test_c.append(c)"""
 
         return (train_a, train_c, test_a, test_c)
 
-    def train_model(self, data):
-        X = tf.placeholder(tf.float32, [28,28,1])
+    def train_model(self, rdd_join):
+        X = tf.placeholder(tf.float32, [1,10])
         # load data, 60K trainset and 10K testset
-        mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+        #mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-        """# 1. Define Variables and Placeholders
-        X = tf.placeholder(tf.float32, [None, 28, 28, 1]) #the first dimension (None) will index the images
+        #a1 = tf.placeholder(list, data.collect())
+        #print a1
+        # 1. Define Variables and Placeholders
+        X = tf.placeholder(tf.float32, [None, 1, 10]) #the first dimension (None) will index the images
         # Y_ = ?
-        Y_ = tf.placeholder(tf.float32,[None,10]) # one hot encoding
+        Y_ = tf.placeholder(tf.float32, [None, 1, 10]) # one hot encoding
          # correct answers
         # Weights initialised with small random values between -0.2 and +0.2
         # 200, 100, 60, 30 and 10 neurons for each layer
-        W1 = tf.Variable(tf.truncated_normal([784, 200], stddev=0.1)) # 784 = 28 * 28
-        B1 = tf.Variable(tf.zeros([200]))
-        W2 = tf.Variable(tf.truncated_normal([200, 100], stddev=0.1)) # 784 = 28 * 28
-        B2 = tf.Variable(tf.zeros([100]))
-        W3 = tf.Variable(tf.truncated_normal([100, 60], stddev=0.1)) # 784 = 28 * 28
+        W1 = tf.Variable(tf.truncated_normal([1, 10], stddev=0.1)) # 784 = 28 * 28
+        B1 = tf.Variable(tf.zeros([3]))
+        W2 = tf.Variable(tf.truncated_normal([1, 10], stddev=0.1)) # 784 = 28 * 28
+        B2 = tf.Variable(tf.zeros([3]))
+        """W3 = tf.Variable(tf.truncated_normal([100, 60], stddev=0.1)) # 784 = 28 * 28
         B3 = tf.Variable(tf.zeros([60]))
         W4 = tf.Variable(tf.truncated_normal([60, 30], stddev=0.1)) # 784 = 28 * 28
         B4 = tf.Variable(tf.zeros([30]))
         W5 = tf.Variable(tf.truncated_normal([30, 10], stddev=0.1)) # 784 = 28 * 28
-        B5 = tf.Variable(tf.zeros([10]))
+        B5 = tf.Variable(tf.zeros([10]))"""
         # 2. Define the model
         # XX = ?
-        XX = tf.reshape(X, [-1, 784]) # flattening images
+        XX = tf.reshape(X, [-1, 1]) # flattening images
 
         # Y = Wx + b
         ######## SIGMOID activation func #######
@@ -134,13 +146,14 @@ class InfluxTensorflow():
         # Y3 = tf.nn.sigmoid(tf.matmul(Y2, W3) + B3)
         # Y4 = tf.nn.sigmoid(tf.matmul(Y3, W4) + B4)
         ######## ReLU activation func #######
+
         Y1 = tf.nn.relu(tf.matmul(XX, W1) + B1)
-        Y2 = tf.nn.relu(tf.matmul(Y1, W2) + B2)
-        Y3 = tf.nn.relu(tf.matmul(Y2, W3) + B3)
-        Y4 = tf.nn.relu(tf.matmul(Y3, W4) + B4)
+        #Y2 = tf.nn.relu(tf.matmul(Y1, W2) + B2)
+        #Y3 = tf.nn.relu(tf.matmul(Y2, W3) + B3)
+        #Y4 = tf.nn.relu(tf.matmul(Y3, W4) + B4)
 
         # Ylogits = ?
-        Ylogits = tf.matmul(Y4, W5) + B5
+        Ylogits = tf.matmul(Y1, W2) + B2 # (Y4, W5) + B5
 
         # Y = tf.nn.?(Ylogits)
         Y = tf.nn.softmax(Ylogits)
@@ -166,7 +179,8 @@ class InfluxTensorflow():
         train_step = optimizer.minimize(cross_entropy)
 
         # initialize and train
-        init = tf.initialize_all_variables()
+        #init = tf.initialize_all_variables()
+        init = tf.global_variables_initializer()
         sess = tf.Session()
         sess.run(init)
 
@@ -177,6 +191,8 @@ class InfluxTensorflow():
         test_a = []
         test_c = []
 
+        sess.run(feed_dict=rdd_join)
+
         training_iter = 1000
         epoch_size = 100
 
@@ -184,23 +200,25 @@ class InfluxTensorflow():
             test = False
             if i % epoch_size == 0:
                 test = True
-            a, c, ta, tc = training_step(i, test, test)
+            a, c, ta, tc = training_step(i, test, test, X, Y_, rdd_join)
             train_a += a
             train_c += c
             test_a += ta
-            test_c += tc"""
+            test_c += tc
 
         print X
         return []
 
+    def train_model_test(self, rdd_join):
+        training_data = rdd_join.collect()
+        data_initializer = tf.placeholder(dtype=tf.float32,
+                                    shape=[1, 3])
+        input_data = tf.Variable(data_initializer, trainable=False, collections=[])
+        sess = tf.Session()
+        sess.run(input_data.initializer, feed_dict={data_initializer: training_data})
+
     def load_data_into_tensorflow(self, data):
-        return self.train_model(data_)
-
-    def add_column_name_to_data(self, values, value):
-        return [val+[value] for val in values]
-
-    def extract_time_list(self, values):
-        return [val[0] for val in values]
+        return self.train_model_test(data)
 
     def join_rdd(self, rdd1_, rdd2_):
         rdd_ = rdd1_.join(rdd2_) #.collectAsMap() # .reduceByKey(lambda x,y : x+y)
@@ -209,22 +227,9 @@ class InfluxTensorflow():
         else:
             return rdd1_
 
-    def main(self):
-
-        time1 = 1490706976000000000
-        time2 = 1490706999000000000
-#        query = "{0} limit {1} offset {2}".format(self.query_g, limit, offset)
-        query = "{0} where time > {1} and time < {2} limit 3".format(self.query_g, time1, time2)
-        results_g = self.query_batch(query, db="graphite")
-
-        query = "{0} where time > {1} and time < {2} limit 3".format(self.query_t, time1, time2)
-        results_t = self.query_batch(query, db="telegraf")
-        values_t =(results_t.raw['series'])[1]['values']
-
-        sc = SparkContext()
-
+    def join_graphite_metrics(self, results_g, sc):
         count = 0
-        for res_g in results_g.raw['series'][0:]:
+        for res_g in results_g.raw['series'][0:4]:
             values_g = res_g['values']
             name_g = res_g['name']
 
@@ -237,30 +242,48 @@ class InfluxTensorflow():
                 rdd_join = self.join_rdd(rdd_join, rdd1_)
                 pass
             count += 1
-        print rdd_join.collect()
-        #print rdd_join.coalesce(1).collect()   # .glom()
+            if re.search(r'yarn.Hostname=(.*?)\.',name_g,re.I|re.S):
+                hostname = re.search(r'yarn.Hostname=(.*?)\.',name_g,re.I|re.S).group(1)
+                #print "hostname:",hostname
+        return rdd_join
 
-        """for res_t in results_t.raw['series'][:2]:
+    def join_telegraf_metrics(self, results_t, rdd_join, sc):
+        for res_t in results_t.raw['series'][:2]:
             values_t = res_t['values']
             name_t = res_t['name']
             rdd1 = sc.parallelize(values_t)
             rdd1_ = rdd1.map(lambda x: (x[0], tuple(x[1:])))
             rdd_join = self.join_rdd(rdd_join, rdd1_)
-        print rdd_join.collect()"""
+        return rdd_join
 
-        #columns = a['columns']  # value and time
-        if re.search(r'yarn.Hostname=(.*?)\.',name_g,re.I|re.S):
-            hostname = re.search(r'yarn.Hostname=(.*?)\.',name_g,re.I|re.S).group(1)
-            #print "hostname:",hostname
-            #values = self.add_column_name_to_data(values=values, value=hostname)
-            time_list = self.extract_time_list(values_g)
+    def main(self):
+
+        time1 = 1490706976000000000
+        time2 = 1490706999000000000
+        query = "{0} where time > {1} and time < {2} limit 3".format(self.query_g, time1, time2)
+        results_g = self.query_batch(query, db="graphite")
+
+        query = "{0} where time > {1} and time < {2} limit 3".format(self.query_t, time1, time2)
+        results_t = self.query_batch(query, db="telegraf")
+        values_t =(results_t.raw['series'])[1]['values']
+
+        sc = SparkContext()
+
+        rdd_join = self.join_graphite_metrics(results_g, sc)
+        #rdd_join = self.join_telegraf_metrics(results_t, rdd_join, sc)
+        rdd_join = (rdd_join.map(lambda x : [x[0]] + list(x[1])))
+        print rdd_join.collect()
+        #print rdd_join.coalesce(1).glom().collect()   # .glom()
 
         result = self.load_data_into_tensorflow(rdd_join)
 
 
 if __name__ == '__main__':
-    username = 'abc'
-    password = 'xyz'
-    indbtf = InfluxTensorflow('localhost', 8086, username, pasword, 'graphite')
+    username = 'adminuser'
+    password = 'adminpw'
+    f = open('/home/vagrant/config.txt', 'rb')
+    info = (f.read()).split("\n")
+    username = info[0]
+    password = info[1]
+    indbtf = InfluxTensorflow('localhost', 8086, username, password, 'graphite')
     indbtf.main()
-
